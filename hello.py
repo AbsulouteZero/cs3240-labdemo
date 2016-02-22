61c6a6f6052a5b2370d95e3e3b225b7c7ada3d2c
Ahnaf Hassan <ath2fc@virginia.edu>
2016-02-22 14:29:01 -0500

    Initial commit

--- hello.py
+++ hello.py
@@ -1,7 +1,6 @@
 __author__ = 'ath2fc'
 
-def greeting(msg):
-    print(msg)
+from helper import greeting
 
 if __name__ == "__main__":
-    greeting("Hello")
+    greeting('Hello')
---
+++ helper.py
@@ -0,0 +1,4 @@
+__author__ = "ath2fc"
+
+def greeting(msg):
+    print(msg)
